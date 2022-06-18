fc8afc8e6939aec0401e63039fca6078b8dc7d2e
Matthew <ikirik59@gmail.com>
2022-06-18 17:39:20 +0300

    Add basic test setup and example

--- FileStorage.Tests/FileListTests.cs
+++ FileStorage.Tests/FileListTests.cs
@@ -1,6 +1,33 @@
-﻿namespace FileStorage.Tests;
+﻿using System.IO.Abstractions.TestingHelpers;
+using Xunit;
+
+namespace FileStorage.Tests;
 
 public class FileListTests
 {
-    
+    private class MockDto
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+    }
+    private const string fileDirectory = @"c:\data\";
+    private const string fileName = "mock_dtos.jsonl";
+    private const string filePath = @"c:\data\mock_dtos.jsonl";
+
+    [Fact]
+    public void CreatesFileIfNotExists()
+    {
+        // Arrange
+        var fileSystem = new MockFileSystem();
+        
+        // Act
+        var _sut = new FileList<MockDto>(
+            fileDirectory,
+            fileName,
+            fileSystem);
+        
+        //Assert
+        bool fileExists = fileSystem.File.Exists(filePath);
+        Assert.True(fileExists);
+    }
 }
