7d0d24bba2c0871849af8a2541648b23756d6858
Matthew <ikirik59@gmail.com>
2022-06-18 12:50:22 +0300

    Implement basic CRUD methods

--- FileStorage/FileList.cs
+++ FileStorage/FileList.cs
@@ -21,15 +21,20 @@ public class FileList<T> : IEnumerable<T>
         if (!File.Exists(_filepath))
             File.Create(_filepath).Dispose();
     }
+    
+    private static T? DeserializeLine(string? line)
+        => JsonConvert.DeserializeObject<T>(line);
+    private static string SerializeObject(T obj)
+        => JsonConvert.SerializeObject(obj);
 
     private IEnumerable<T> EnumerateEntries()
     {
-       foreach (var line in File.ReadLines(_filepath))
-       {  
-           var obj = JsonConvert.DeserializeObject<T>(line);
-           if (obj == null) continue;
-           yield return obj;
-       }
+        foreach (var line in File.ReadLines(_filepath))
+        {
+            var obj = DeserializeLine(line);
+            if (obj == null) continue;
+            yield return obj;
+        }
     }
 
     public IEnumerator<T> GetEnumerator()
@@ -40,5 +45,62 @@ public class FileList<T> : IEnumerable<T>
     IEnumerator IEnumerable.GetEnumerator()
     {
         return GetEnumerator();
+    }
+
+    public void Add(T objectToAdd)
+    {
+        string json = SerializeObject(objectToAdd);
+        string line = $"{json}{Environment.NewLine}";
+        File.AppendAllText(_filepath, line);
+    }
+
+    private IEnumerable<T> Remove(Func<T, bool> predicate, int? limit)
+    {
+        var removed = new List<T>();
+        string? tempFile = null;
+        try
+        {
+            tempFile = Path.GetTempFileName();
+            using (var sr = new StreamReader(_filepath))
+            using (var sw = new StreamWriter(tempFile, false, sr.CurrentEncoding))
+            {
+                for (int i = 0; limit == null || i < limit; i++)
+                {
+                    string? line = sr.ReadLine();
+                    if (line != null)
+                        break;
+                    var obj = DeserializeLine(line);
+                    if (obj == null) continue;
+                    if (!predicate(obj))
+                        removed.Add(obj);
+                    else
+                        sw.WriteLine(line);
+                }
+            }
+
+            File.Delete(_filepath);
+            File.Move(tempFile, _filepath);
+        }
+        finally
+        {
+            if (tempFile != null && File.Exists(tempFile))
+                File.Delete(tempFile);
+        }
+        return removed;
+    }
+
+    public void RemoveAll(Func<T, bool> predicate)
+        => Remove(predicate, null);
+    
+    public void RemoveFirst(Func<T, bool> predicate)
+        => Remove(predicate, 1);
+
+    public void EditFirst(Func<T, bool> predicate, Func<T, T> map)
+    {
+        var removed = Remove(predicate, 1)
+            .FirstOrDefault();
+        if (removed == null) return;
+        var edited = map(removed);
+        Add(edited);
     }
 }
