15ce063aca19a139b956a3490e6e07378e6c9770
Matthew <ikirik59@gmail.com>
2022-06-18 12:03:38 +0300

    Add FileList stub

---
+++ FileStorage/FileList.cs
@@ -0,0 +1,21 @@
+﻿namespace FileStorage;
+
+public class FileList
+{
+    private readonly string _path;
+    private readonly string _filepath;
+
+    public FileList(string path, string filename)
+    {
+        _path = path;
+        _filepath = Path.Combine(_path, filename);
+        EnsureFileExists();
+    }
+
+    private void EnsureFileExists()
+    {
+        Directory.CreateDirectory(_path);
+        if (!File.Exists(_filepath))
+            File.Create(_filepath).Dispose();
+    }
+}
