3b37e51b37a969f329318efda3cc55a8b3866b8d
Matthew <ikirik59@gmail.com>
2022-06-18 18:21:04 +0300

    Make constructor lightweight

--- FileStorage/FileList.cs
+++ FileStorage/FileList.cs
@@ -6,33 +6,41 @@ namespace FileStorage;
 
 public class FileList<T> : IEnumerable<T>
 {
-    private readonly string _path;
     private readonly IFileSystem _fileSystem;
-    private readonly string _filepath;
+    private string _filepath;
 
-    public FileList(string path, string filename, IFileSystem fileSystem)
+    private string FilePath
     {
-        _path = path;
+        get
+        {
+            EnsureFileExists();
+            return _filepath;
+        }
+    }
+
+    public FileList(string filepath, IFileSystem fileSystem)
+    {
+        _filepath = filepath;
         _fileSystem = fileSystem;
-        _filepath = Path.Combine(_path, filename);
-        EnsureFileExists();
     }
 
     private void EnsureFileExists()
     {
-        _fileSystem.Directory.CreateDirectory(_path);
-        if (!_fileSystem.File.Exists(_filepath))
-            _fileSystem.File.Create(_filepath).Dispose();
+        if (_fileSystem.File.Exists(_filepath)) return;
+        string directory = _fileSystem.Path.GetDirectoryName(_filepath);
+        _fileSystem.Directory.CreateDirectory(directory);
+        _fileSystem.File.Create(_filepath).Dispose();
     }
-    
+
     private static T? DeserializeLine(string? line)
         => JsonConvert.DeserializeObject<T>(line);
+
     private static string SerializeObject(T obj)
         => JsonConvert.SerializeObject(obj);
 
     private IEnumerable<T> EnumerateEntries()
     {
-        foreach (var line in _fileSystem.File.ReadLines(_filepath))
+        foreach (var line in _fileSystem.File.ReadLines(FilePath))
         {
             var obj = DeserializeLine(line);
             if (obj == null) continue;
@@ -54,7 +62,7 @@ public class FileList<T> : IEnumerable<T>
     {
         string json = SerializeObject(objectToAdd);
         string line = $"{json}{Environment.NewLine}";
-        _fileSystem.File.AppendAllText(_filepath, line);
+        _fileSystem.File.AppendAllText(FilePath, line);
     }
 
     private IEnumerable<T> Remove(Func<T, bool> predicate, int? limit)
@@ -64,7 +72,7 @@ public class FileList<T> : IEnumerable<T>
         try
         {
             tempFile = Path.GetTempFileName();
-            using (var sr = new StreamReader(_filepath))
+            using (var sr = new StreamReader(FilePath))
             using (var sw = new StreamWriter(tempFile, false, sr.CurrentEncoding))
             {
                 for (int i = 0; limit == null || i < limit; i++)
@@ -81,20 +89,21 @@ public class FileList<T> : IEnumerable<T>
                 }
             }
 
-            _fileSystem.File.Delete(_filepath);
-            _fileSystem.File.Move(tempFile, _filepath);
+            _fileSystem.File.Delete(FilePath);
+            _fileSystem.File.Move(tempFile, FilePath);
         }
         finally
         {
             if (tempFile != null && _fileSystem.File.Exists(tempFile))
                 _fileSystem.File.Delete(tempFile);
         }
+
         return removed;
     }
 
     public void RemoveAll(Func<T, bool> predicate)
         => Remove(predicate, null);
-    
+
     public void RemoveFirst(Func<T, bool> predicate)
         => Remove(predicate, 1);
 
