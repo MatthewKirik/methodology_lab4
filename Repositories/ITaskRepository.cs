d5e8a5e841b087fd18b5406d299b9b75574fda47
Matthew <ikirik59@gmail.com>
2022-06-18 13:09:03 +0300

    Add basic task service interface describing required logic

--- Repositories/ITaskRepository.cs
+++ Repositories/ITaskRepository.cs
@@ -4,8 +4,9 @@ namespace Repositories;
 
 public interface ITaskRepository
 {
-    IEnumerable<Task> GetTasks();
-    IEnumerable<Task> GetTasks(Func<TaskDto, bool> filter);
-    IEnumerable<Task> RemoveTask(int id);
-    IEnumerable<Task> EditTask(int id, TaskDto newValue);
+    IEnumerable<TaskDto> GetTasks();
+    IEnumerable<TaskDto> GetTasks(Func<TaskDto, bool> filter);
+    void RemoveTask(int id);
+    void EditTask(int id, TaskDto newValue);
+    void AddTask(TaskDto task);
 }
