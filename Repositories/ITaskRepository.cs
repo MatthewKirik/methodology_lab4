5f95bfb06ac658011e48db124a0a16652bc6a2dd
Matthew <ikirik59@gmail.com>
2022-06-18 13:00:48 +0300

    Add basic task repository interface

---
+++ Repositories/ITaskRepository.cs
@@ -0,0 +1,11 @@
+﻿using DataTransfer.Objects;
+
+namespace Repositories;
+
+public interface ITaskRepository
+{
+    IEnumerable<Task> GetTasks();
+    IEnumerable<Task> GetTasks(Func<TaskDto, bool> filter);
+    IEnumerable<Task> RemoveTask(int id);
+    IEnumerable<Task> EditTask(int id, TaskDto newValue);
+}
