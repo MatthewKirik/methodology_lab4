e516ddf74e8c94fabec99036f885251e61208335
Matthew <ikirik59@gmail.com>
2022-06-18 18:21:53 +0300

    Change naming to fit convention

--- FileStorage.Tests/FileListTests.cs
+++ FileStorage.Tests/FileListTests.cs
@@ -12,7 +12,7 @@ public class FileListTests
         public string Text { get; set; }
     }
 
-    private const string filePath = @"c:\data\mock_dtos.jsonl";
+    private const string FilePath = @"c:\data\mock_dtos.jsonl";
 
     [Fact]
     public void CreatesFileIfNotExists()
@@ -20,13 +20,13 @@ public class FileListTests
         // Arrange
         var fileSystem = new MockFileSystem();
         var _sut = new FileList<MockDto>(
-            filePath,
+            FilePath,
             fileSystem);
         var mockObject = new MockDto {Id = 12, Text = "Hello"};
 
         // Act
         _sut.Add(mockObject);
-        bool fileExists = fileSystem.File.Exists(filePath);
+        bool fileExists = fileSystem.File.Exists(FilePath);
 
         //Assert
         fileExists.Should().BeTrue();
