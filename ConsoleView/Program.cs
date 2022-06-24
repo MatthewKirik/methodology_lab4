4576ac8e9269fcd54486c8931c5827ff42c28750
gurug-prog <ivanmysterys@gmail.com>
2022-06-24 11:46:30 +0300

    Use non-static args processor class

--- ConsoleView/Program.cs
+++ ConsoleView/Program.cs
@@ -3,21 +3,22 @@ using Services.Implementations;
 
 
 var helpPage =
-	"Argument help page:" +
-	"-a,  --add          { Title }  { Text: optional } { Deadline (yyyy-mm-dd): optional }" +
-	"-e.  --edit         { Id } { Title } { Text: optional } { Deadline (yyyy-mm-dd): optional }" +
-	"-r,  --remove       { Id }" +
-	"-m,  --mark         { Id }" +
-	"-s,  --show" +
-	"-so, --show-ordered" +
-	"-se, --show-expired";
+	"Argument help page:\n" +
+	"-a,  --add          { Title }  { Text: optional } { Deadline (yyyy-mm-dd): optional }\n" +
+	"-e.  --edit         { Id } { Title } { Text: optional } { Deadline (yyyy-mm-dd): optional }\n" +
+	"-r,  --remove       { Id }\n" +
+	"-m,  --mark         { Id }\n" +
+	"-s,  --show\n" +
+	"-so, --show-ordered\n" +
+	"-se, --show-expired\n";
 
 try
 {
 	var filePath = "./data.jsonl";
 	var taskRepo = new TaskRepository(filePath);
 	var service = new TaskService(taskRepo);
-	ArgumentProcessor.Parse(service, args);
+	var argsProcessor = new ArgumentProcessor(service);
+	argsProcessor.Parse(args);
 }
 catch (Exception ex)
 {
